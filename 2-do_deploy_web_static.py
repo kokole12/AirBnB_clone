a16b37d7d4a997a7fb1d6f8ba524204e6b91306e
kokol12 <kokoleismail886@gmail.com>
2023-04-07 22:17:47 +0300

    fabric file to deploy a .tgz file to server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" fabric scrit to deploy the .tgz file created"""
+from fabric.api import *
+import os.path
+from os.path import exists
+
+
+def do_deploy(archive_path):
+	if exists(archibe_path) is None:
+		return False
+	try:
+		file_name = archive_path.split("/")[-1]
+		name = file_name.split(".")[0]
+		path = /data/web_static/releases/
+		put(archive_path, '/tmp/')
+		run("mkdir -p {}{}/".format(path, name)
+		run("tar -xvf /tmp/ {} -C {}{}/".format(file_name, path, name))
+		run("rm -rf /tmp/{}".format(filename))
+		run("mv {}{}/web_static/* {}{}".format(path, name, path,name))
+		run("rm -rf {}{}/web_static".fornat(path, name))
+		run("rm -rf /data/web_stattic/current")
+		run("ln -s {}{}/ /data/web_static/current".format(path, name)
+		return True
+	except Exception as e:
+		return False	
